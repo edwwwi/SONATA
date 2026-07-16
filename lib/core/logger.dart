28c94031e9f31fd326139da8f3d04e75bd16b385
edwwwi <edwinjoy777@gmail.com>
2026-07-17 00:45:09 +0530

    aff

--- lib/core/logger.dart
+++ lib/core/logger.dart
@@ -13,25 +13,33 @@ class AppLogger {
     return File(join(logsDir.path, 'application.log'));
   }
 
-  static Future<void> log(String screen, String message, {dynamic exception, StackTrace? stackTrace}) async {
+  //LOGGER
+  static Future<void> log(
+    String screen,
+    String message, {
+    dynamic exception,
+    StackTrace? stackTrace,
+  }) async {
     try {
       final file = await _logFile;
-      final timestamp = DateFormat('yyyy-MM-dd HH:mm:ss').format(DateTime.now());
-      
+      final timestamp = DateFormat(
+        'yyyy-MM-dd HH:mm:ss',
+      ).format(DateTime.now());
+
       final buffer = StringBuffer();
       buffer.writeln('[$timestamp] [$screen] $message');
-      
+
       if (exception != null) {
         buffer.writeln('Exception: $exception');
       }
       if (stackTrace != null) {
         buffer.writeln('StackTrace:\n$stackTrace');
       }
-      
+
       buffer.writeln('----------------------------------------');
-      
+
       await file.writeAsString(buffer.toString(), mode: FileMode.append);
-      
+
       // Also print to console during development
       print(buffer.toString());
     } catch (e) {
